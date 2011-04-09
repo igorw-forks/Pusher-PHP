f68e3ac4f08a6fecdb30dcef1efa63e894270cfb
Igor Wiedler <igor@wiedler.ch>
2011-04-09 21:05:53 +0200

    initial PHPUnit file layout

---
+++ tests/Pusher/PusherTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Pusher-PHP library.
+ *
+ * (c) Squeeks <squeek@cpan.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pusher\Tests;
+
+use Pusher;
+
+/**
+ * Pusher test cases.
+ *
+ * @author Igor Wiedler <igor@wiedler.ch>
+ */
+class PusherTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $pusher = new Pusher('authkey', 'secret', 'appid');
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * This file is part of the Pusher-PHP library.
+ *
+ * (c) Squeeks <squeek@cpan.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__.'/../lib/Pusher.php';
